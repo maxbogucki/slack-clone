09c903a80250bb3be15bd3a922c56cc2901083b2
maxbogucki <bogucki.max@gmail.com>
2025-08-31 13:28:53 -0500

    db connected & env.js completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-console.log("mongo uri:", ENV.MONGO_URI);
-
 app.listen(5001, () => {
   console.log(`Server started on port ${ENV.PORT}`);
+  connectDB();
 });
