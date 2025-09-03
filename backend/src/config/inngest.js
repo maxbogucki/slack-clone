81703e1621a7ef04932df1864712d7e812cf7d7a
maxbogucki <bogucki.max@gmail.com>
2025-09-03 16:41:59 -0500

    stream & sentry added

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
@@ -22,6 +23,12 @@ const syncUser = inngest.createFunction(
     };
 
     await User.create(newUser);
+
+    await upsertStreamUser({
+        id: newUser.clerkId.toString(),
+        name: newUser.name,
+        image: newUser.image,
+    });
   }
 );
 
@@ -32,6 +39,8 @@ const deleteUserFromDB = inngest.createFunction(
     await connectDB();
     const { id } = event.data;
     await User.findOneAndDelete({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
   }
 );
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,32 @@
+import "../instrument.mjs"; // Import Sentry instrumentation as the first line
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { inngest, functions } from "./config/inngest.js";
 import { serve } from "inngest/express";
+import chatRoutes from "./routes/chat.route.js";
+
+import * as Sentry from "@sentry/node";
 
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(clerkMiddleware()); // req.auth will be available in the request object
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.get("/debug-sentry", (req, res) => {
+  throw new Error("This is a test error for Sentry!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+
+Sentry.setupExpressErrorHandler(app);
+
 
 const startServer = async () => {
   try {
@@ -22,7 +34,6 @@ const startServer = async () => {
     if (ENV.NODE_ENV !== "production") {
       app.listen(5001, () => {
         console.log(`Server started on port ${ENV.PORT}`);
-        connectDB();
       });
     }
   } catch (error) {
