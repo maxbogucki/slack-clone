885910c5e65bd4318fb555c7e5040c21fc150a92
maxbogucki <bogucki.max@gmail.com>
2025-09-04 12:25:48 -0500

    AuthProvider & tools setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,21 +9,25 @@ import { Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 
+import * as Sentry from "@sentry/react";
+
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
+
 const App = () => {
   return (
     <>
       <SignedIn>
-        <Routes>
+        <SentryRoutes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<Navigate to ={"/"} replace />} />
-        </Routes>
+          <Route path="/auth" element={<Navigate to={"/"} replace />} />
+        </SentryRoutes>
       </SignedIn>
 
-      <SignedOut >
-        <Routes>
+      <SignedOut>
+        <SentryRoutes>
           <Route path="/auth" element={<AuthPage />} />
-          <Route path="*" element={<Navigate to ={"/auth"} replace />} />
-        </Routes>
+          <Route path="*" element={<Navigate to={"/auth"} replace />} />
+        </SentryRoutes>
       </SignedOut>
     </>
   );
